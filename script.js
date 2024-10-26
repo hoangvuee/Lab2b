ffbd7a55209a2002463844eeb615a5f18c76762d
hoangvuee <22130328@st.hcmuaf.edu.vn>
2024-10-26 16:59:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+// Mảng chứa đường dẫn đến các hình ảnh
+const images = [
+    '/images/main/image-1.jpg',
+    '/images/main/image-2.jpg',
+    '/images/main/image-3.jpg'
+];
+
+// Vị trí hiện tại của ảnh
+let currentIndex = 0;
+
+// Lấy phần tử .imgcontent
+const imgContent = document.querySelector('.imgcontent');
+
+const circles = document.querySelectorAll('.circlechangeimg');
+
+// Hàm cập nhật ảnh
+function updateImage() {
+    imgContent.style.backgroundImage = `url(${images[currentIndex]})`;
+    updateCircle();
+
+}
+function updateCircle() {
+    circles.forEach((circle, index) => {
+        if (index === currentIndex) {
+            circle.style.backgroundColor = 'white';
+        } else {
+            circle.style.backgroundColor = 'black';
+        }
+    });
+}
+
+// Xử lý sự kiện cho nút buttonpre
+document.querySelector('.buttonpre').addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateImage();
+});
+
+// Xử lý sự kiện cho nút buttonnext
+document.querySelector('.buttonnext').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateImage();
+});
+
+// Khởi tạo ảnh ban đầu
+updateImage();
